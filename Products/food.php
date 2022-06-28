61e4ec3dcbc474070e818660f569d384b3ade828
Evanflow89 <ivanbaragone@gmail.com>
2022-06-28 17:24:30 +0200

    commit

--- Products/food.php
+++ Products/food.php
@@ -1,8 +1,17 @@
 <?php
 
-class Employee extends Products{
+require_once__DIR__ .'/Products.php';
+
+class food extends Products{
     public $type;
-    public %expire_date;
+    public $expire_date;
+
+    public function__construct($name, $price, $quantity, $animal_type); {
+        parent::__construct($name, $price, $quantity, $animal_type);
+        this->type = $_type;
+        this->expire_date = $_expire_date;
 }
+}
+
 
 ?>
--- Products/toy.php
+++ Products/toy.php
@@ -1,8 +1,14 @@
 <?php
+require_once__DIR__ .'/Products.php';
 
-class Employee extends Products {
+class toy extends Products {
     public $type;
     public $size;
-}
 
+    public function__construct($name, $price, $quantity, $animal_type); {
+        parent::__construct($name, $price, $quantity, $animal_type);
+        this->type = $_type;
+        this->size = $_size;
+}
+}
 ?>
