74b56e35ead095b0ff00a40106f9dc910627da1b
Evanflow89 <ivanbaragone@gmail.com>
2022-06-28 16:54:11 +0200

    commit products php

--- Products.php
+++ Products.php
@@ -4,8 +4,16 @@ class Products {
     public $name;
     public $price;
     public $quantity;
-    public &animal_type;
-}
+    public $animal_type;
 
+
+    public function__construct($name, $price, $quantity, $animal_type); {
+
+    this->name = $_name;
+    this->price = $_price;
+    this->quantity = $_quantity;
+    this->animal_type= $animal_type;
+    }
+}
 
 ?>
